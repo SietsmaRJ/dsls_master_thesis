446d210353cbd7f68ae4842aff2d37fee9bf5ad4
rjsietsma <r.j.sietsma@st.hanze.nl>
2020-03-18 11:17:07 +0100

    Updated venv_installer.sh

--- venv_installer.sh
+++ venv_installer.sh
@@ -12,7 +12,6 @@ else
 fi
 echo "Checking and installing required packages"
 source "./venv/bin/activate"
-pip install scipy
 pip install -r requirements.txt
 echo "All done! Do not forget to activate the venv using:"
 echo "source './venv/bin/activate' (you can use ctrl+shift+c to copy the command)"
